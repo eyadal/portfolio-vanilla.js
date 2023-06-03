ce27aaf1583dc8430c97cbe2b7609bf4a552e950
eyadal <31968847+eyadal@users.noreply.github.com>
2023-06-03 19:04:44 +0200

    'display specific github repositories in projects element.'

--- main.js
+++ main.js
@@ -1,69 +1,55 @@
-// const getAllReposAndPrintOnScreen = async () => {
-//   const reposWrapper = document.querySelector('.box')
-//   try {
-//     const res = await fetch('https://api.github.com/users/eyadal/repos')
-//     const data = await res.json()
-//     `
-//           <div class="card">
-//           <h5>${repos}</h5>
-//           <p>description</p>
-//           <a class="button" href="${repo.html_url}">Repo</a>
-
-//           </div>
-//           `
-
-//     reposWrapper.innerHTML = reposHtml
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-
-// getAllReposAndPrintOnScreen()
-
 const getAllReposAndPrintOnScreen = async () => {
   const reposWrapper = document.querySelector('.box')
   try {
     const res = await fetch('https://api.github.com/users/eyadal/repos')
     const data = await res.json()
+    const repos = [
+      {
+        name: data[0].name,
+        url: data[0].html_url,
+        description: data[0].description,
+      },
+      {
+        name: data[16].name,
+        url: data[16].html_url,
+        description: data[16].description,
+      },
+      {
+        name: data[17].name,
+        url: data[17].html_url,
+        description: data[17].description,
+      },
+      {
+        name: data[22].name,
+        url: data[22].html_url,
+        description: data[22].description,
+      },
+      {
+        name: data[24].name,
+        url: data[24].html_url,
+        description: data[24].description,
+      },
+    ]
 
-    //name, url, description
-
-    console.log(data[0].name, data[0].url, data[0].description)
-    console.log(data[16])
-    console.log(data[17])
-    console.log(data[22])
-    console.log(data[24])
-    const repos = [data[0], data[16], data[17], data[22], data[24]]
-    for (repo = 0; repo < repos.length; repo++) {
-      const repoURL = repo.url
-      const repoName = repo.name
-      const repoDescription = repo.description`
-      <div class="box">
-      <div class="card">
-        <h5>${repoName}</h5>
-            <p>${repoDescription}</p>
-        <a class="button" href="${repoURL}">Repository</a>
-      </div>
-    </div>
-    
-      `
-    }
+    repos.map((repo) => ({
+      name: repo.name,
+      url: repo.html_url,
+      description: repo.description,
+    }))
 
     let reposHtml = ''
-    data.forEach((repo) => {
-      const repoURL = repo.html_url
+    repos.map((repo) => {
+      const repoURL = repo.url
       const repoName = repo.name
       const repoDescription = repo.description
 
-      const repoCard = `
-        <div class="box">
-          <div class="card">
-            <h5>${repoName}</h5>
-                <p>${repoDescription}</p>
-            <a class="button" href="${repoURL}">Repository</a>
-          </div>
+      let repoCard = `
+        <div class="card">
+          <h5>${repoName}</h5>
+          <p class="par">${repoDescription}</p>
+          <a class="button" href="${repoURL}">Repository</a>
         </div>
-        `
+      `
 
       reposHtml += repoCard
     })
